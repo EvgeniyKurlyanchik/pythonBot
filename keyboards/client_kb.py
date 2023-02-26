6a1e948362f5a2a8229186fb01e4328bfdf413e9
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2023-02-26 03:05:52 +0300

    bot

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -3,6 +3,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 b1 = KeyboardButton('/режим_работы')
 b2 = KeyboardButton('/Адрес')
 b3 = KeyboardButton('/меню')
+# b4 = KeyboardButton('Поделится номером', request_contact=True)
+# b5 = KeyboardButton('Геолокация', request_location=True)
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard= True)
 kb_client.insert(b1).insert(b2).insert(b3)
+# kb_client.insert(b1).insert(b2).insert(b3).insert(b4).insert(b5)
